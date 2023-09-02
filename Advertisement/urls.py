ee64a44bf3180f32a997b2a7c681f28ac4d753c5
Ilkin Hidayev <ilkin.hidayev@gmail.com>
2023-09-02 03:53:31 +0200

    Current status with working images and adding

--- Advertisement/urls.py
+++ Advertisement/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from .import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('list/', views.list_ads, name='list_ads'),
     path('edit/<int:ad_id>/', views.edit_ad, name='edit_ad'),
     path('manage/', views.manage_ads, name='manage_ads'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- TaxiAds/settings.py
+++ TaxiAds/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
+
 
 # Application definition
 
@@ -120,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

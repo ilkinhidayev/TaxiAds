18b0411eb122e60ca5bbebf3ef6a6282cca565ba
Ilkin Hidayev <ilkin.hidayev@gmail.com>
2023-09-02 05:10:25 +0200

    Location adding to form

--- Advertisement/models.py
+++ Advertisement/models.py
@@ -5,3 +5,9 @@ class Ad(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='ads/')
     creation_date = models.DateTimeField(auto_now_add=True)
+    LOCATION_CHOICES = (
+        ('yasamal', 'Yasamal'),
+        ('nizami', 'Nizami'),
+        # Other regions
+    )
+    location = models.CharField(max_length=100, choices=LOCATION_CHOICES, null=True, blank=True)
